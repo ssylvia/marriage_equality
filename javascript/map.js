ce9c97c141c538e323be8ac4cd55f35675c62c4a
leebock <lee@leebocksongs.com>
2012-09-20 14:10:09 -0400

    restricting zoom in the proper manner (not the in-and-back-out way).

--- javascript/map.js
+++ javascript/map.js
@@ -22,6 +22,27 @@ var toBoolean = function (str) {
   }
 };
 
+
+var _lods = [
+	{"level" : 0, "resolution" : 156543.033928, "scale" : 591657527.591555},
+	{"level" : 1, "resolution" : 78271.5169639999, "scale" : 295828763.795777},
+	{"level" : 2, "resolution" : 39135.7584820001, "scale" : 147914381.897889},
+	{"level" : 3, "resolution" : 19567.8792409999, "scale" : 73957190.948944},
+	{"level" : 4, "resolution" : 9783.93962049996, "scale" : 36978595.474472},
+	{"level" : 5, "resolution" : 4891.96981024998, "scale" : 18489297.737236},
+	{"level" : 6, "resolution" : 2445.98490512499, "scale" : 9244648.868618},
+	{"level" : 7, "resolution" : 1222.99245256249, "scale" : 4622324.434309},
+	{"level" : 8, "resolution" : 611.49622628138, "scale" : 2311162.217155},
+	{"level" : 9, "resolution" : 305.748113140558, "scale" : 1155581.108577},
+	{"level" : 10, "resolution" : 152.874056570411, "scale" : 577790.554289},
+	{"level" : 11, "resolution" : 76.4370282850732, "scale" : 288895.277144},
+	{"level" : 12, "resolution" : 38.2185141425366, "scale" : 144447.638572},
+	{"level" : 13, "resolution" : 19.1092570712683, "scale" : 72223.819286},
+	{"level" : 14, "resolution" : 9.55462853563415, "scale" : 36111.909643},
+	{"level" : 15, "resolution" : 4.77731426794937, "scale" : 18055.954822},
+	{"level" : 16, "resolution" : 2.38865713397468, "scale" : 9027.977411}
+];
+
 function initMap(){
 	patchID();
       
@@ -124,7 +145,8 @@ function createMap(){
 			mapOptions: {
 				slider: true,
 				nav: false,
-				wrapAround180:true
+				wrapAround180:true,
+				lods: _lods.slice(3,(i == 0 ? 14 : 7))
 			},
 			ignorePopups:false,
 			bingMapsKey: configOptions.bingmapskey
@@ -156,15 +178,6 @@ function createMap(){
 					$("#legend0").append("<img src='images/Tracts.small.circles.jpg'></img>");			
 				} else {
 					$("#legend0").append("<img src='images/Tracts.medium.circles.jpg'></img>");			
-				}
-				
-				if (map.id == "mapDiv0") {
-					// check allowable scale for dot map
-					if (map.getLevel() > 12) map.setLevel(12);
-				} else if (map.id == "mapDiv1") {
-					if (map.getLevel() > 7) map.setLevel(7);
-				} else if (map.id == "mapDiv2") {
-					if (map.getLevel() > 6) map.setLevel(6);
 				}
 				
 			});
